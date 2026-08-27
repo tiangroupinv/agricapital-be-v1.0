5ffa470b866ff2e7aca4e8777c498dba9e7379ff
Rancy21 <rancyontsandaga@gmail.com>
2026-08-27 18:57:43 +0200

    fix: update naming

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const app = require("./app");
-const connectDB = require("./database");
+const { connect } = require("./database");
 
 const PORT = process.env.PORT || 5000;
 
 async function start() {
-  await connectDB();
+  await connect();
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 }
 
